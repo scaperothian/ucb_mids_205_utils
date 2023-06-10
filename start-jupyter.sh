271477c2ebcdb019587b728c43a736b0e99f3e60
Your Name <you@example.com>
2023-06-10 10:21:24 -0700

    added another pause because I think jupyter is not coming up before I start trying to access the tmux session.

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -11,10 +11,13 @@ tmux send-keys -t postgres "docker-compose up" Enter
 tmux detach-client -s postgres
 
 echo "pause while postgres comes up."
-sleep 4
+sleep 2
 
 tmux new-session -d -s jupyter
 tmux send-keys -t jupyter "cd /home/w205/docker/clusters/anaconda_postgres" Enter
 tmux send-keys -t jupyter "docker-compose exec anaconda jupyter notebook --notebook-dir=/user --ip='*' --port=8888 --no-browser --allow-root" Enter
+
+echo "pause while postgres comes up."
+sleep 2
 
 tmux attach-session -t jupyter
