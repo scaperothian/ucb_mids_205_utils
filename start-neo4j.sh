857e5085b13fc8826bcb5f7255459131da6dafd3
Your Name <you@example.com>
2023-06-26 05:57:37 -0700

    adding script for neo4j.

--- start-neo4j.sh
+++ start-neo4j.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 # Create a new tmux session
-tmux new-session -d -s postgres
+tmux new-session -d -s neo4j 
 
 # Send a command to the session
-tmux send-keys -t postgres "cd /home/w205/docker/clusters/anaconda_postgres" Enter
-tmux send-keys -t postgres "docker-compose up" Enter
+tmux send-keys -t neo4j "cd /home/w205/docker/clusters/anaconda_neo4j" Enter
+tmux send-keys -t neo4j "docker-compose up" Enter
 
 # Detach from the session
-tmux detach-client -s postgres
+tmux detach-client -s neo4j
+
+echo "pause while neo4j comes up."
+sleep 2
 
 echo "pause while postgres comes up."
 sleep 2
 
 tmux new-session -d -s jupyter
-tmux send-keys -t jupyter "cd /home/w205/docker/clusters/anaconda_postgres" Enter
+
+tmux send-keys -t jupyter "cd /home/w205/docker/clusters/anaconda_neo4j" Enter
 tmux send-keys -t jupyter "docker-compose exec anaconda jupyter notebook --notebook-dir=/user --ip='*' --port=8888 --no-browser --allow-root" Enter
 
-echo "pause while postgres comes up."
+echo "pause while jupyter comes up."
 sleep 2
 
 tmux attach-session -t jupyter
---
+++ start-postgres.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create a new tmux session
+tmux new-session -d -s postgres
+
+# Send a command to the session
+tmux send-keys -t postgres "cd /home/w205/docker/clusters/anaconda_postgres" Enter
+tmux send-keys -t postgres "docker-compose up" Enter
+
+# Detach from the session
+tmux detach-client -s postgres
+
+echo "pause while postgres comes up."
+sleep 2
+
+tmux new-session -d -s jupyter
+
+tmux send-keys -t jupyter "cd /home/w205/docker/clusters/anaconda_postgres" Enter
+tmux send-keys -t jupyter "docker-compose exec anaconda jupyter notebook --notebook-dir=/user --ip='*' --port=8888 --no-browser --allow-root" Enter
+
+echo "pause while jupyter comes up."
+sleep 2
+
+tmux attach-session -t jupyter
+
